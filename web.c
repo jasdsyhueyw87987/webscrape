194602632d039f0d04a769f71a85e81b32d21df0
jasdsyhueyw87987 <jasdsyhueyw87987@gmail.com>
2024-10-30 10:14:17 -0700

    Update web.c

--- web.c
+++ web.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <curl/curl.h>
 
+// Callback function to handle incoming data
+size_t write_callback(void *ptr, size_t size, size_t nmemb, void *userdata) {
+    size_t total_size = size * nmemb;
+    fwrite(ptr, size, nmemb, (FILE *)userdata); // Write data to the file
+    return total_size;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <URL>\n", argv[0]);
+        return 1;
+    }
+
+    CURL *curl;
+    CURLcode res;
+
+    // Initialize libcurl
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    if (curl) {
+        // Set the URL for the request
+        curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+
+        // Set the callback function to handle data
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, stdout); // Output to stdout
+
+        // Perform the request
+        res = curl_easy_perform(curl);
+
+        // Check for errors
+        if (res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        }
+
+        // Clean up
+        curl_easy_cleanup(curl);
+    }
+
+    // Clean up libcurl
+    curl_global_cleanup();
+
+    return 0;
+}
